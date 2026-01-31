c045606b68562db9ed6a3d40c93c70936f949067
José Herrera <jherrerauemee@gmail.com>
2026-01-30 19:51:42 -0500

    test 30-1-2026

--- config/settings.py
+++ config/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER', default=''),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': config('DB_ENGINE'),
+        # 'NAME': config('DB_NAME'),
+        # 'USER': config('DB_USER', default=''),
+        # 'PASSWORD': config('DB_PASSWORD', default=''),
+        # 'HOST': config('DB_HOST', default='localhost'),
+        # 'PORT': config('DB_PORT', default='5432'),
     }
 }
 
